8501aceeee7486c270897b44e9f10bd28858ca6d
Marc <markeks@outlook.de>
2020-02-24 16:19:14 +0100

    inital test commit

--- data/install.sql
+++ data/install.sql
@@ -10,6 +10,7 @@ INSERT INTO `core_form_tab` (`Tab_ID`, `form`, `title`, `subtitle`, `icon`, `cou
 INSERT INTO `core_form_field` (`Field_ID`, `type`, `label`, `fieldkey`, `tab`, `form`, `class`, `url_view`, `url_list`, `show_widget_left`, `allow_clear`, `readonly`, `tbl_cached_name`, `tbl_class`, `tbl_permission`) VALUES
 (NULL, 'partial', 'Stamp', 'worktime_stamp', 'worktime-stamp', 'worktime-single', 'col-md-12', '', '', '0', '1', '0', '', '', '');
 
+
 --
 -- create stamp table
 --
